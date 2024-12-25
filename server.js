c2448407f788b97c900de75101da730bf35829b5
0Thomas1 <thomas2001114@gmail.com>
2024-12-25 13:28:13 -0800

    added comments to functions

--- server.js
+++ server.js
@@ -4,22 +4,27 @@ const port = 3000;
 const root = require("path").join(__dirname, "client");
 const MongoDB = require("./utils/mongodb.js");
 
+//serve the static files
 app.use(express.static(root));
 
+//connect to the MongoDB database
 MongoDB.connectToMongoDB().then(() => {
   console.log("Connected to MongoDB");
-  
 });
 
+//serve the index.html file
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
+
+//get the kanban boards for the user
 app.get("/api/boards", (req, res) => {
   MongoDB.getKanbanBoards("thomas").then((boards) => {
     res.send(boards);
   });
 });
 
+//start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
